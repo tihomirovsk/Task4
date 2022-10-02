4bbd21a9d4cd6b2db0b39a016683dd949cb42183
tihomirovsk <pesokote@gmail.com>
2022-10-02 19:22:16 +0300

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int numberA = ReadInt("Введите число A ");
+int numberB = ReadInt("Введите число B ");
+ToDegree(numberA, numberB);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
