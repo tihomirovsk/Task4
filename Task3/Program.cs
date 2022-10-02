363c3e8085f5c80b440d5d3856833ab7a697107d
tihomirovsk <pesokote@gmail.com>
2022-10-02 19:47:35 +0300

    Task3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int lenArray = ReadInt("Введите длинну массива ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
